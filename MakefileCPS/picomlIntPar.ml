4573ba837117343f7e06923e2077726061d0f01a
Umang Mathur <umang.sunilmathur@gmail.com>
2015-12-13 15:18:45 -0600

    Indentation

--- MakefileCPS/picomlIntPar.ml
+++ MakefileCPS/picomlIntPar.ml
@@ -14,47 +14,53 @@ let _ =
         then print_endline "\nWelcome to the Student parser \n"
         else ());
     let rec loop gamma mem = 
-        try
+    try
+        (*
         let lexbuf = Lexing.from_channel stdin
+        *)
+
+        let lexbuf = Lexing.from_channel (open_in "testing.txt")
         in 
         (
             if is_interactive 
                 then (print_string "> "; flush stdout)
-                else ());
+                else ()
+        );
+        (
+        try
+            let dec = main 
+                (fun lb -> match Picomllex.token lb 
+                    with 
+                    | EOF -> raise Picomllex.EndInput
+                    | r -> r
+                ) lexbuf 
+            in 
+            match infer_dec gather_dec_ty_substitution gamma dec 
+            with 
+            | None -> 
+                (
+                print_string "\ndoes not type check\n";
+                loop gamma mem
+                )
+            | Some (Proof(hyps,judgement)) ->
+                (
+                match check_tail_recursion dec 
+                with true -> print_string "Tail Recursive!\n"; loop gamma mem
+                | false -> print_string "Not Tail Recursive!\n"; loop gamma mem
+                )  
+        with Failure s -> 
             (
-                try
-                let dec = main (fun lb -> match Picomllex.token lb 
-                with 
-                | EOF -> raise Picomllex.EndInput
-                | r -> r)
-                lexbuf 
-                in 
-                match infer_dec gather_dec_ty_substitution gamma dec 
-                with
-                | None -> 
-                    (
-                    print_string "\ndoes not type check\n";
-                    loop gamma mem
-                    )
-
-                | Some (Proof(hyps,judgement)) ->
-                    (
-                    match check_tail_recursion dec 
-                    with true -> print_string "Tail Recursive!\n"; loop gamma mem
-                    | false -> print_string "Not Tail Recursive!\n"; loop gamma mem
-                    )  
-                with Failure s -> 
-                    (
-                    print_newline();
-                    print_endline s;
-                    print_newline();
-                    loop gamma mem
-                    )
-                | Parsing.Parse_error ->
-                    (
-                    print_string "\ndoes not parse\n";
-                    loop gamma mem
-                    )
+            print_newline();
+            print_endline s;
+            print_newline();
+            loop gamma mem
             )
-        with Picomllex.EndInput -> exit 0
-        in (loop [] []) ;;
+        | Parsing.Parse_error ->
+            (
+            print_string "\ndoes not parse\n";
+            loop gamma mem
+            )
+        )
+    with Picomllex.EndInput -> exit 0
+
+in (loop [] []) ;;
